40612009b331a8ef5d422070bd7f625a3bfa9dad
rallen2lk <105309855+rallen2lk@users.noreply.github.com>
2022-05-14 22:27:29 +0300

    Add files via upload

--- check.py
+++ check.py
@@ -1,89 +1,57 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import requests
+import plotly.express as px
 from cryptocmd import CmcScraper
 from plotly import graph_objs as go
 
-
-import pandas as pd
-
-
-tic = st.sidebar.text_input("Enter a ticker")
-download = st.checkbox('I wrote a ticker')
-scraper = CmcScraper('GMT')
-df = scraper.get_dataframe()
-st.dataframe(df)
-st.sidebar.markdown("<p class='big-font'>Settings</font></p>", unsafe_allow_html=True)
-if download:
-    selected_ticker = tic #Can change into other crypto
-    period = int(st.sidebar.number_input('Number of days to predict:', min_value=0, max_value=1000000, value=365, step=1))
-    training_size = int(st.sidebar.number_input('Training set (%) size:', min_value=10, max_value=100, value=100, step=5)) / 100
-    @st.cache
-    def load_data(selected_ticker):
-	    init_scraper = CmcScraper(selected_ticker)
-	    df = init_scraper.get_dataframe()
-	    min_date = pd.to_datetime(min(df['Date']))
-	    max_date = pd.to_datetime(max(df['Date']))
-	    return min_date, max_date
-
-    data_load_state = st.sidebar.text('Loading data...')
-    min_date, max_date = load_data(selected_ticker)
-    data_load_state.text('Loading data... done!')
-    scraper = CmcScraper(selected_ticker)
-    data = scraper.get_dataframe()
-
-    st.subheader('Historical data from Coinmarketcap.com')
-    st.write(data.head())
-
-    ### Plot data----
-    def plot_raw_data():
-	    fig = go.Figure()
-	    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="Close"))
-	    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-	    st.plotly_chart(fig)
-    plot_raw_data()
-
-else:
-    st.write('Please enter a ticker')
-
-values = st.slider(
-     'Select a range of values',
-     0, 100, (25, 75))
-st.write('Values:', values)
+with st.echo(code_location='below'):
+    tic = st.text_input("Enter a ticker")
+    download = st.checkbox('I wrote a ticker')
+    if download:
+        selected_ticker = tic
+        @st.cache
+        ### FROM: (https://github.com/sfwong46/streamlit-crypto-demo/blob/main/streamlit-crypto-demo.py)
+        def load_data(selected_ticker):
+            init_scraper = CmcScraper(selected_ticker)
+            df = init_scraper.get_dataframe()
+            min_date = pd.to_datetime(min(df['Date']))
+            max_date = pd.to_datetime(max(df['Date']))
+            return (min_date, max_date)
 
 
-def get_data():
-    data_url = (
-        "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-        "master/data/movie_metadata.csv"
-    )
-    return (
-        pd.read_csv(data_url)
-            .dropna(subset=["title_year"])
-            .assign(
-            title_year=lambda x: pd.to_datetime(
-                x["title_year"], format="%Y"
-            )
-        )
-    )
+        data_load_state = st.sidebar.text('Starting load data...')
+        min_date, max_date = load_data(selected_ticker)
+        data_load_state.text('Done!')
+        scraper = CmcScraper(selected_ticker)
+        data = scraper.get_dataframe()
+
+        st.subheader('Historical data from Coinmarketcap.com')
+        st.write(data.head())
 
 
-df = get_data()
-director = st.selectbox(
-    "Director", df["director_name"].value_counts().iloc[:10].index
-)
+        def plot_raw_data():
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="Close"))
+            fig.layout.update(title_text='Price of selected ticker', xaxis_rangeslider_visible=True)
+            st.plotly_chart(fig)
+        plot_raw_data()
+        ### END FROM
 
-df_selection = df[lambda x: x["director_name"] == director]
-df_selection
+    
 
-web3.eth.get.balances('')
+    else:
+        st.write('Please enter a ticker')
 
-def print_hello(name='Anna'):
+    projects = ['BTC', "GMT", 'SAND', 'APE', 'ZIL']
+    ### FROM: (https://discuss.streamlit.io/t/checkbox-to-download-some-data-and-trigger-button/4160/4)
+    tip = [st.sidebar.checkbox(project, key = project) for project in projects]
+    save =  [project for project, checked in zip(projects, tip) if checked]
+    ### END FROM
 
-    st.write(f"## Hello, {name}!")  # Press Ctrl+F8 to toggle the breakpoint.
+    if st.button('Download data'):
+        for project in save:
+            scraper = CmcScraper(project)
+            data = scraper.get_dataframe()
+            plot_raw_data()
 
-print_hello()
 
-
-#
