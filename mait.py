4ac5ea7c2e500faa2ece2d3e6e52fbafd117f913
bobbobr <105309855+bobbobr@users.noreply.github.com>
2022-06-12 19:07:00 +0300

    Update mait.py

--- mait.py
+++ mait.py
@@ -17,8 +17,6 @@ s = pd.read_csv('check.csv')
 model = LinearRegression()
 model.fit(s[["Rate"]], s["Min"])
 
-model.fit(s[["Min"]], s["Votes"])
-
 number=st.number_input('Insert a number.')
 st.subheader(model.predict(pd.DataFrame([[number]], columns=["Min"]))[0])
 
